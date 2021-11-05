534a85a738ecf1857adf13c32de6274d1e7b432c
LisaWeijers <lisa.weijers@rhdhv.com>
2021-11-05 13:08:34 +0100

    finish coupling afsluitmiddel functie

--- examples/wbd/afsluitmiddel_koppeling.py
+++ examples/wbd/afsluitmiddel_koppeling.py
@@ -8,6 +8,7 @@ import numpy as np
 import itertools
 from operator import itemgetter
 import pandas as pd
+import os
 
 def ckdnearest_linestring(gdfA, gdfB): #, gdfB_cols=['Place']):
     A = np.concatenate(
@@ -24,15 +25,15 @@ def ckdnearest_linestring(gdfA, gdfB): #, gdfB_cols=['Place']):
     gdf = pd.concat(
         [gdfA, gdfB_rename.iloc[[*idx]].reset_index(drop=True),
          pd.Series(dist, name='dist_line')], axis=1)
-    return gdf
+    return gdf.set_index('CODE')
 
 def ckdnearest_points(gdA, gdB):
     nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
     nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
     btree = cKDTree(nB)
     dist, idx = btree.query(nA, k=1)
-    gdB_nearest = gdB.iloc[idx].drop(columns="geometry").reset_index(drop=True)
-    gdB_nearest.columns = [f"{col}_near_point" for col in gdB_nearest.columns]
+    gdB_nearest = gdB.iloc[idx].reset_index(drop=True) #.drop(columns="geometry") (removed from middle)
+    gdB_nearest.columns = [f"{col}_point" for col in gdB_nearest.columns]
     gdf = pd.concat(
         [
             gdA.reset_index(drop=True),
@@ -40,21 +41,81 @@ def ckdnearest_points(gdA, gdB):
             pd.Series(dist, name='dist_point')
         ],
         axis=1)
-    return gdf
+    return gdf.set_index('CODE')
 
+def closest(row, max_dist):
+    min_dist = min([row['dist_line'], row['dist_point']])
+    if min_dist > max_dist:
+        return "0 - 99 - 0"
+    elif min_dist == row['dist_line']:
+        return f"{row['CODE_line']} - {row['dist_line']} - line"
+    else:
+        return f"{row['CODE_point']} - {row['dist_point']} - point"
+
+def closest_geometry(row):
+    closest_type = row['type_closest']
+    if closest_type == 'line':
+        return row['geometry_line']
+    elif closest_type == 'point':
+        return row['geometry_point']
+    else:
+        return row['geometry']
+
+
+def find_closest(gdf_afsluitmiddel, gdf_stuw, gdf_duiker, output_folder, search_dist = 10, export_check=False):
+    """
+    This function calculates the nearest neighbor between afsluitmiddel/stuw & afsluitmiddel/duiker using KD Trees.
+    WARNING: KD Trees can make mistakes in finding the NN, because the tree divides the spatial grid in clusters,
+    it will only find NN's in the same clusters. Locations at the edge might experience mistakes in assigning the NN.
+    - culverts are line elements, they are simplified to a set of points for the purpose of finding the NN
+
+    After finding nearest neighbour between afsluitmiddel/stuw & afsluitmiddel/duiker, the distances of both findings
+    are compared. The closest nearest neighbour is assigned as closest structure, unless there is no NN within the
+    search_dist. Default search distance = 10.
+    """
+    closest_stuw = ckdnearest_points(gdf_afsluitmiddel, gdf_stuw)
+    closest_duiker = ckdnearest_linestring(gdf_afsluitmiddel, gdf_duiker)
+
+    # join closest stuw & closest duiker
+    duiker_cols = [col for col in closest_duiker.columns if col.endswith('_line')]
+    all_close = closest_stuw.join(closest_duiker[duiker_cols])
+
+    all_close['temp'] = all_close.apply(lambda x: closest(x, search_dist), axis=1)
+    all_close[['code_closest', 'dist_closest', 'type_closest']] = all_close.temp.str.split(" - ", expand=True)
+    all_close = all_close.astype({'dist_closest': float})
+    all_close['code_closest'] = all_close['code_closest'].replace({'0': None})
+    all_close['dist_closest'] = all_close['dist_closest'].replace({'99': None})
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    if export_check:
+        df_match = all_close.copy()
+        df_match.geometry = df_match.apply(lambda x: closest_geometry(x), axis=1)
+        df_match.geometry = df_match.buffer(0.1)
+        df_match = df_match.drop(columns=['geometry_line', 'geometry_point'])
+        df_match.to_file(os.path.join(output_folder, 'kunstwerk_match_check.shp'))
+        df_match.to_file(os.path.join(output_folder, 'kunstwerk_match_check.gpkg'), driver='GPKG')
+
+    all_close = all_close.drop(columns=['geometry_line', 'geometry_point'])
+    all_close.to_file(os.path.join(output_folder, 'afsluitmiddel_koppeling.shp'))
+    all_close.to_file(os.path.join(output_folder, 'afsluitmiddel_koppeling.gpkg'), driver='GPKG')
+    return all_close
 
 
 if __name__ == '__main__':
     gdf_afsluitmiddel = read_featureserver(
         r'https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Kunstwerken/FeatureServer', 0)
-
     gdf_stuw = read_featureserver(
         r'https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Kunstwerken/FeatureServer', 8)
     gdf_duiker = read_featureserver(
         r'https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Kunstwerken/FeatureServer', 12)
 
-    closest_stuw = ckdnearest_points(gdf_afsluitmiddel, gdf_stuw)
-    print(closest_stuw)
-
-    closest_duiker = ckdnearest_linestring(gdf_afsluitmiddel, gdf_duiker)
-    print(closest_duiker)
+    closest = find_closest(
+        gdf_afsluitmiddel=gdf_afsluitmiddel,
+        gdf_stuw=gdf_stuw,
+        gdf_duiker=gdf_duiker,
+        output_folder='output/afsluitmiddel',
+        search_dist=10,
+        export_check=True
+    )
