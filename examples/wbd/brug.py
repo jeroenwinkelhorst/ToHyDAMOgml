801c4d339b17781a1b34494f8818447cfebb0b63
LisaWeijers <lisa.weijers@rhdhv.com>
2021-11-26 09:28:18 +0100

    make XYZ profiel for bridges. TO DO check output crosssection profiles, since they use the same function

--- examples/wbd/brug.py
+++ examples/wbd/brug.py
@@ -1,25 +1,50 @@
 import geopandas as gpd
 from tohydamogml.read_database import read_featureserver
 from shapely.ops import snap
+from dwarsprofiel_xyz import _make_xyz
+import tqdm
 
-def determine_bottom_height(brug, legger):
+def make_bridge_profile(brug, legger):
     line = []
     buffer_size = 0
     while len(line) is 0:
         buffer_size += 0.5
         line = legger[legger.geometry.intersects(brug.geometry.buffer(buffer_size))]
+        if buffer_size > 5:
+            print(f'No waterline found, skipped bridge: {brug["CODE"]}')
+            return None
     line = line.iloc[0]
     snapped = snap(brug.geometry, line.geometry, 1)
-    dist = line.geometry.project(snapped, normalized=True)
-    height = line['WS_BH_BOVENSTROOMS_L'] - dist * (line['WS_BH_BOVENSTROOMS_L']-line['WS_BH_BENEDENSTROOMS_L'])
-    return height
+    dist_normalized = line.geometry.project(snapped, normalized=True)
+    bottom = line['WS_BH_BOVENSTROOMS_L'] - dist_normalized * (line['WS_BH_BOVENSTROOMS_L']-line['WS_BH_BENEDENSTROOMS_L'])
+    geom_length = line.geometry.length
+    dist = dist_normalized * geom_length
+    if dist + 5 < geom_length:
+        help_dist = dist + 5
+    else:
+        help_dist = dist - 5
+    height = brug['HOOGTEONDE'] - bottom
+    if height < 0.1:
+        height = 2
+    xyz = _make_xyz(row=line, distance_main=dist, distance_help=help_dist,
+                    bottom_level=bottom, bottom_width=brug['WS_DOORSTR'],
+                    talud_l=0.01, talud_r=0.01, total_depth = height)
+    return xyz
 
 if __name__ == '__main__':
-    gdf_brug = read_featureserver(
-        r'https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Kunstwerken/FeatureServer', 4)
     gdf_legger = read_featureserver(
         r'https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Legger_Vigerend/FeatureServer', 18)
+    gdf_brug = gpd.read_file(r'c:\Users\908367\Box\BH8519 WBD DHYDRO\BH8519 WBD DHYDRO WIP\00_Exchange\Waterschap Brabantse Delta\20210611_aanvullendegegevens\Leveren_aan_RHDHV\dwarsprofielen\Brugprofielen.shp')
+    mask = gpd.read_file(r"c:\local\TKI_WBD\aanvullende_data\Aa_of_Weerijs_v2.shp")
+    bruggen = gdf_brug[gdf_brug.intersects(mask.unary_union)]
 
-    brug1 = gdf_brug.iloc[0]
-    h1 = determine_bottom_height(brug1, gdf_legger)
-    print(h1)
+    # problem = make_bridge_profile(bruggen.iloc[28], gdf_legger)
+
+    profiles = []
+    for i, brug in tqdm.tqdm(bruggen.iterrows(), total=len(bruggen)):
+        profile = make_bridge_profile(brug, gdf_legger)
+        profiles.append(profile)
+
+    # profiles = bruggen.apply(lambda bridge: make_bridge_profile(bridge, gdf_legger), axis=1)
+    print(profiles)
+
--- examples/wbd/dwarsprofiel_xyz.py
+++ examples/wbd/dwarsprofiel_xyz.py
@@ -9,7 +9,16 @@ import tqdm
 def make_profile(gdf):
     new_points = gpd.GeoDataFrame(crs = 'EPSG:28992')
     for i, row in tqdm.tqdm(gdf.iterrows(), total=len(gdf)):
-        up_line = _make_xyz(row, 'upstream')
+        length = row.geometry.length
+        if length > 20:
+            up_dist_main, up_dist_help = 5, 10
+            down_dist_main, down_dist_help = length-5, length-10
+        else:
+            up_dist_main, up_dist_help = length*0.25, length*0.5
+            down_dist_main, down_dist_help = length*0.75, length*0.5
+        up_line = _make_xyz(row=row, distance_main=up_dist_main, distance_help=up_dist_help,
+                            bottom_level=row['WS_BH_BOVENSTROOMS_L'], bottom_width=row['WS_BODEMBREEDTE_L'],
+                            talud_l=row['WS_TALUD_LINKS_L'], talud_r=row['WS_TALUD_RECHTS_L'], total_depth = 10)
         up_points = list(up_line.coords)
         for j in range(len(up_points)):
             new = gpd.GeoDataFrame(geometry=[Point(up_points[j])],
@@ -21,7 +30,9 @@ def make_profile(gdf):
                                          'RuwheidsWaardeLaag': [30],
                                          'RuwheidsWaardeHoog': [20]})
             new_points = new_points.append(new)
-        down_line = _make_xyz(row, 'downstream')
+        down_line = _make_xyz(row=row, distance_main=down_dist_main, distance_help=down_dist_help,
+                              bottom_level=row['WS_BH_BENEDENSTROOMS_L'], bottom_width=row['WS_BODEMBREEDTE_L'],
+                              talud_l=row['WS_TALUD_LINKS_L'], talud_r=row['WS_TALUD_RECHTS_L'], total_depth = 10)
         down_points = list(down_line.coords)
         for j in range(len(up_points)):
             new = gpd.GeoDataFrame(geometry=[Point(down_points[j])],
@@ -35,50 +46,26 @@ def make_profile(gdf):
             new_points = new_points.append(new)
     return new_points
 
-def _make_xyz(row, up_or_down: str = 'upstream', profile_dist: float = 5, total_depth = 10):
+def _make_xyz(row, distance_main, distance_help, bottom_level, bottom_width, talud_l, talud_r, total_depth = 10):
     # Set up basic profile parameters: bottom width & total width based on talud
-    bottom_width = row['WS_BODEMBREEDTE_L']
     if math.isnan(bottom_width):
         bottom_width = 2
         logging.info(f"Profielen maken: {row['CODE']} geen bodembreedte gevonden, bodembreedte op 2 gezet.")
-    total_width = bottom_width + total_depth * row['WS_TALUD_LINKS_L'] + total_depth * row['WS_TALUD_RECHTS_L']
+    total_width = bottom_width + total_depth * talud_l + total_depth * talud_r
     if math.isnan(total_width):
         total_width = bottom_width + 2 * total_depth * 2
         logging.info(f"Profielen maken: {row['CODE']} geen taluds gevonden, talud op 2 gezet.")
-    # Set up basic profile parameters: bottom level up or downstream. If either cannot be found, use the other, or 0
-    if up_or_down.lower().startswith('up'):
-        level_col = ['WS_BH_BOVENSTROOMS_L', 'WS_BH_BENEDENSTROOMS_L']
-    else:
-        level_col = ['WS_BH_BENEDENSTROOMS_L', 'WS_BH_BOVENSTROOMS_L']
-    bottom_level = row[level_col[0]]
-    if math.isnan(bottom_level):
-        bottom_level = row[level_col[1]]
-        if math.isnan(bottom_level):
-            bottom_level = 0
-            logging.info(f"Profielen maken: {row['CODE']} geen bodemhoogte gevonden, bodemhoogte op 0 gezet.")
-        else:
-            logging.info(f"Profielen maken: {row['CODE']} een van de bodemhoogtes niet gevonden, namelijk {level_col[0]}, "
-                         f"bodemhoogte van {level_col[1]} gebruikt.")
+
     upper_level = bottom_level + total_depth
 
     # Profile will be created using a short sub-segment of the source-line-geometry.
     l = row['geometry'].length
-    logging.info(f'Profielen maken: {up_or_down}, {row["CODE"]}, lengte: {l}, bodembreedte: {bottom_width}, '
+    logging.info(f'Profielen maken: {row["CODE"]}, op afstand: {distance_main} van lengte: {l}, '
+                 f'bodembreedte: {bottom_width}, '
                  f'bodemhoogte: {bottom_level}, totale diepte: {total_depth}')
-    # If the total line is long enough, use a sub-segment that is 1 meter long,
-    # otherwise make the segment on 1/5th and 2/5th of the total length of the line
-    if l > (profile_dist * 2 + 1):
-        dist1 = profile_dist
-        dist2 = profile_dist - 1
-    else:
-        dist1 = l * 0.2
-        dist2 = l * 0.4
-    if up_or_down.lower().startswith('down'):
-        dist1 = dist1 * -1
-        dist2 = dist2 * -1
 
-    point1 = row['geometry'].interpolate(dist1)
-    point2 = row['geometry'].interpolate(dist2)
+    point1 = row['geometry'].interpolate(distance_main)
+    point2 = row['geometry'].interpolate(distance_help)
     baseline = LineString([point1, point2])
 
     left_inner = baseline.parallel_offset(bottom_width/2, 'left')
@@ -95,12 +82,14 @@ def _make_xyz(row, up_or_down: str = 'upstream', profile_dist: float = 5, total_
     return profile_line
 
 if __name__ == '__main__':
-    # mask = r"c:\local\TKI_WBD\aanvullende_data\Aa_of_Weerijs_v2.shp"
+    mask = gpd.read_file(r"c:\local\TKI_WBD\aanvullende_data\Aa_of_Weerijs_v2.shp")
     print(r'reading feature server...')
     gdf = read_featureserver(r"https://maps.brabantsedelta.nl/arcgis/rest/services/Extern/Legger_Vigerend/FeatureServer",
                              layer_index="18")
+    gdf = gdf[gdf.drop(columns='SHAPE').intersects(mask.unary_union)]
+
     print(r'feature server read!')
     # gdf = gpd.clip(gdf, mask)
     new_points = make_profile(gdf)
-    new_points.to_file(r'output/dwarsprofiel/dwp_punten.gpkg', driver='GPKG')
-    new_points.to_file(r'output/dwarsprofiel/dwp_punten.shp')
+    new_points.to_file(r'output/dwarsprofiel/dwp_punten_2.gpkg', driver='GPKG')
+    new_points.to_file(r'output/dwarsprofiel/dwp_punten_2.shp')
